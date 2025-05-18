d81be5438a919d70d8fa7e4c7bec2c81f45a138d
sagarad31 <sagarad31q@gmail.com>
2025-05-18 22:14:24 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+const text =
+  "An aspiring computer engineer and passionate web developer based in Nepal.";
+const introEl = document.getElementById("intro");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+let i = 0;
+function type() {
+  if (i < text.length) {
+    introEl.textContent += text.charAt(i);
+    i++;
+    setTimeout(type, 45); // typing speed (ms per character)
+  }
+}
+window.addEventListener("DOMContentLoaded", type);
